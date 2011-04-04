ec79741eb0c63ea1b43efb080ff88585bb20af6a
okm <hi@heavilyinvolved.com>
2011-04-04 15:32:07 -0400

    updated README, MANIFEST.in, and setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name                = 'servicebrowser',
-    version             = '0.1.8',
+    name                = 'fulcrum',
+    version             = '0.1.0',
     description         = 'Data browser and basic REST interface for Django.',
     author              = 'Kieran Lynn',
     author_email        = 'kieran@octothorpstudio.com',
     license             = 'Closed Source',
-    url                 = 'http://pocms.potiondesign.com/packages/servicebrowser/',
+    url                 = 'https://github.com/octothorp/django-fulcrum',
     packages            = find_packages(),
     include_package_data = True,
     zip_safe            = False
