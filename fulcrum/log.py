4a872f54458961b77a1844868ec993d8f75c8846
okm <hi@heavilyinvolved.com>
2011-04-13 14:04:12 -0400

    added logging

---
+++ fulcrum/log.py
@@ -0,0 +1,34 @@
+import logging, sys
+
+INITIALIZED = False
+FORMAT = '[ %(levelname)s ] :: %(message)s'
+
+def debug(msg, *args, **kwargs):
+    logging.debug(msg, *args, **kwargs)
+
+def info(msg, *args, **kwargs):
+    logging.info(msg, *args, **kwargs)
+
+def warning(msg, *args, **kwargs):
+    logging.warning(msg, *args, **kwargs)
+
+def error(msg, *args, **kwargs):
+    logging.error(msg, *args, **kwargs)
+    
+def critical(msg, *args, **kwargs):
+    logging.critical(msg, *args, **kwargs)
+
+
+def init(config):
+    logging.basicConfig(**config)
+
+def basicConfig():
+    return {
+        'level': logging.DEBUG,
+        'format': FORMAT
+    }
+    
+
+if not INITIALIZED:
+    INITIALIZED = True
+    init(basicConfig())
