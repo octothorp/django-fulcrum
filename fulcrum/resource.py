31ef17fc8375f50233ae2d725ec2681ebd0d18fe
okm <hi@heavilyinvolved.com>
2011-04-14 22:11:47 -0400

    added logging to data_url and api_url

--- fulcrum/resource.py
+++ fulcrum/resource.py
@@ -29,6 +29,7 @@ from django.contrib.sites.models import Site
 
 from datastructures import EasyModel
 import schemas
+import log
 
 class Resource(object):
     """
@@ -259,9 +260,11 @@ class Resource(object):
     # -- Helper methods
     
     def data_url(self): # resource_name
+        log.debug('data_url(): {0}'.format(mark_safe(self.name)))
         return mark_safe('{0}'.format(self.name))
     
     def api_url(self): # resource_name/api/
+        log.debug('api_url(): {0}/api'.format(mark_safe(self.name)))
         return mark_safe('{0}/api'.format(self.name))
         
     def schema_urls(self): # resource_name/schema.json
